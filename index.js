eef8cdc95ba921ab04813dcce73eb11d33649e22
sandiegodj <davidjanczyn@gmail.com>
2015-05-13 20:16:45 +0000

    update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var app = express();
 var buf = new Buffer(fs.readFileSync('index.html'),'utf-8');
 
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
-})
+});
 
-app.listen(app.get('port'), function() {
+app.listen(app.get(process.env.PORT || 5000), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
-})
+});
