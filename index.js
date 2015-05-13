362d3c9035349f2502841a0b2210831722e79eee
sandiegodj <davidjanczyn@gmail.com>
2015-05-13 20:38:46 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var buf = new Buffer(256);
+var data = fs.readFilesync("index.html", "utf-8");
 
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('index.html'), 'utf8'));
+  response.send(data);
 });
 
-app.listen(process.env.PORT || 5000);
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
 
