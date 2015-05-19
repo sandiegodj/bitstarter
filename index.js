c3bb014b1469853ed9da17f3a921fe231fb09645
Ubuntu <ubuntu@ip-172-31-30-33.us-west-2.compute.internal>
2015-05-19 17:54:28 +0000

    Edited product desc in index.html

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var data = fs.readFileSync("index.html", "utf-8");
+var htmlfile = "index.html";
 
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(data);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
     console.log("Listening on " + port);
